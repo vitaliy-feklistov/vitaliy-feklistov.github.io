50237a112ea2a2211ba1bbf150d29f5e0f99355a
vitaliy feklistov <feklistov.vitaliy@gmail.com>
2019-03-31 13:38:35 +0300

    3 commit

--- js/common.js
+++ js/common.js
@@ -1,5 +1,24 @@
+
+
+
 $(document).ready(function() {
 
+
+	var imglink;
+	$(".tooltip-img").mouseenter(function() {
+		imglink = $(this).attr("data-imglink");
+		$(this).append('<span class="tooltip-content"><a class="image-link" href="' + imglink + '"><img src="' + imglink + '" alt="..." /></a></span>');
+		$('.image-link').magnificPopup({
+			type: 'image',
+
+		});
+	}).mouseleave(function() {
+		$(this).find(".tooltip-content").remove();
+	});
+
+	$('.image-link').magnificPopup({
+		type: 'image',
+	});
 
 
 });
