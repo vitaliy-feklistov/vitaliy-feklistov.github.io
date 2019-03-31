37cc9724edbfefbb86faab770fe04f55e095f6c7
vitaliy feklistov <feklistov.vitaliy@gmail.com>
2019-03-31 19:37:10 +0300

    fix small images

--- js/common.js
+++ js/common.js
@@ -4,10 +4,12 @@
 $(document).ready(function() {
 
 
-	var imglink;
+	var imglink, $linkNoRash;
 	$(".tooltip-img").mouseenter(function() {
 		imglink = $(this).attr("data-imglink");
-		$(this).append('<span class="tooltip-content"><a class="image-link" href="' + imglink + '"><img src="' + imglink + '" alt="..." /></a></span>');
+		$linkNoRash = imglink;
+		$linkNoRash = $linkNoRash.slice(0, -4);
+		$(this).append('<span class="tooltip-content"><a class="image-link" href="' + imglink + '"><img src="' + $linkNoRash + '-small.jpg" alt="..." /></a></span>');
 		$('.image-link').magnificPopup({
 			type: 'image',
 
